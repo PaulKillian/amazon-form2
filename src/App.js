9b38e2b2bd824d7c744f22ee41870625ee65b5b9
Paul_Killian <psk65lava@gmail.com>
2021-08-04 15:13:12 -0700

    Made form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './amazon-logo.png'
+import { useState } from 'react'
+import './fonts/Ember.ttf'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [submit, setSubmit] = useState('');
+  const [pass, setPass] = useState('')
+  const [passReEnter, setPassReEnter] = useState('')
+  const [redoForm, setRedoForm] = useState(null)
+  const [onChange, setOnChange] = useState('');
+  const [submitComplete, setSubmitComplete] = useState({})
+
+  const handleSubmit = (event) => {
+    if(passReEnter !== pass) {
+      event.preventDefault();
+      setRedoForm(true)
+    } else {
+      event.preventDefault();
+      const form = document.querySelector('form')
+      const formData = new FormData(form);
+      const entries = formData.entries();
+      const data = Object.fromEntries(entries);
+      setSubmitComplete(data)
+      setSubmit('done')
+    }
+  }
+
+  const handleChange = (event) => {
+    if(event.target.name === 'password') {
+      setPass(event.target.value)
+    } 
+    if(event.target.name === 'reenter') {
+      setPassReEnter(event.target.value)
+    }
+    setOnChange(event.target.value)
+  }
+
+  if( submit === '' ) {
+    return (
+      <div className='App'>
+        <main className='App-main'>
+        <h1 className={ redoForm === null ? 'hidden' : null}>
+          There was a problem Passwords must match
+          </h1>
+          <img className={'logo-dimensions'} src={logo} alt='amazon logo'/>
+            <form className={'form-flex'} onSubmit={handleSubmit}>
+              <p>Create account</p>
+              <label className={'padding'}>Your name</label>
+              <input type='text' name='name' required onChange={handleChange}/>
+              <label className={'padding'}>Email</label>
+              <input type='text' name='email' required onChange={handleChange}/>
+              <label className={'padding'}>Password</label>
+              <input type='text' name='password' placeholder='At least 6 characters'required onChange={handleChange}/>
+              <p>&#9432; Passwords must be at least 6 characters.</p>
+              <label>Re-enter password</label>
+              <input type='text' name='reenter' required onChange={handleChange}/>
+              <button className='btn' type={'submit'}>Create your Amazon account</button>
+              <p>By creating an account, you agree to Amazon 
+              <a href='https://www.amazon.com/gp/help/customer/display.html/ref=ap_register_notification_condition_of_use?ie=UTF8&nodeId=508088'> Conditions of Use</a> and 
+              <a href='https://www.amazon.com/gp/help/customer/display.html/ref=ap_register_notification_privacy_notice?ie=UTF8&nodeId=468496'> Privacy Notice.</a></p>
+              <hr></hr>
+              <p>Already have an account? <a href='https://www.amazon.com/ap/signin?clientContext=130-4649631-2400710&openid.return_to=https%3A%2F%2Ftrack.amazon.com%2F&prevRID=YB3M9WPQE6W56KY0SC9X&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=amzn_shippingrecipientcentral_us&openid.mode=checkid_setup&siteState=clientContext%3D133-5959651-2511706%2CsourceUrl%3Dhttps%253A%252F%252Ftrack.amazon.com%252F%2Csignature%3DaIjtVnII3EuhLKiVpJY6BuFeqXYj3D&marketPlaceId=A2BR6UVHX99FEC&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&pageId=amzn_shippingrecipientcentral_us_amazon&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0'>Sign-In</a></p>
+            </form>
+            <hr></hr>
+            <p className={'center-align'}>
+              <a href='https://www.amazon.com/gp/help/customer/display.html/ref=ap_register_notification_condition_of_use?ie=UTF8&nodeId=508088'>Conditions of Use</a>  
+              <a href='https://www.amazon.com/gp/help/customer/display.html/ref=ap_register_notification_privacy_notice?ie=UTF8&nodeId=468496'>Privacy Notice    </a><a href='https://www.amazon.com/gp/help/customer/display.html'>Help</a>
+            </p>
+            <p className={'center-align'}>© 1996-2021, Amazon.com, Inc. or its affiliates</p>
+          </main>
+      </div>
+    )
+  } else {
+    return (
+      <div className='bg'>
+        <h3>Name: <span>{submitComplete.name}</span></h3>
+        <h3>Email: <span>{submitComplete.email}</span></h3>
+        <h3>Password: <span>{submitComplete.password}</span></h3>
+        <h3>Re-Entered Password: <span >{submitComplete.reenter}</span></h3>
+      </div>
+    )
+  }
 }
+
 
 export default App;
